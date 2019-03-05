65e7ef0ea451cbd8b4a229405b4a2cdf77a94eba
Naor <thehecticbyte@gmail.com>
2019-03-05 16:06:11 +0200

    feat: Added card functionality and removed classes - Cards can now be flipped when clicked - Added a card opening limit - Removed unnecessary classes from cards in the index page

--- js/app.js
+++ js/app.js
@@ -1,15 +1,8 @@
-/*
- * Create a list that holds all of your cards
- */
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
+const cards = document.querySelectorAll('.card');
+let openedCards = [];
+ 
+ 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -24,15 +17,30 @@ function shuffle(array) {
 
     return array;
 }
+// Game init function
+function startGame(){
+	for(const card of cards) {
+		card.addEventListener('click', function(e){
+			if(!card.classList.contains('open', 'show')) {
+
+				if(openedCards.length < 2){
+					openedCards.push(card);
+					card.classList.add('open', 'show');	
+					if(openedCards.length == 2){
+						if(openedCards[0].innerHTML == openedCards[1].innerHTML)
+						{
+							openedCards[0].classList.add('match');	
+							openedCards[1].classList.add('match');
+							openedCards = [];			
+						}
+					}
+				}else{
+
+				}
+			}
+		});
+	};
+}
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+startGame();
